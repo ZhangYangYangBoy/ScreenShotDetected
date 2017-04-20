e44646fd260f9d0fe5eecb5a6c3494b6adc01d17
yangyangzhang <zyydeveloper@163.com>
2017-04-20 10:28:02 +0800

    Initial Commit

---
+++ ScreenShotDetected/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScreenShotDetected
+//
+//  Created by Tech-zhangyangyang on 2017/4/20.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScreenShotDetected/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScreenShotDetected
+//
+//  Created by Tech-zhangyangyang on 2017/4/20.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
